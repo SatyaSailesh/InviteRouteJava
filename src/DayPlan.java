5cb5b3a232b1865638875ac28834549e05db3a20
Kotla Satya Sai Sailesh <kotla.sailesh2006@gmail.com>
2026-03-10 10:40:34 +0530

    Update DayPlan.java

--- src/DayPlan.java
+++ src/DayPlan.java
@@ -1,19 +1,12 @@
 import java.util.List;
 import java.util.ArrayList;
-
-/**
- * DayPlan.java
- * Holds the complete schedule for a single day.
- */
 public class DayPlan {
-
     public static class Visit {
         public String name;
         public double x, y;
         public String arrivalTime;
         public String departureTime;
         public int    travelMinFromPrev;
-
         public Visit(String name, double x, double y,
                      String arrival, String departure, int travelMin) {
             this.name               = name;
@@ -24,17 +17,14 @@ public class DayPlan {
             this.travelMinFromPrev  = travelMin;
         }
     }
-
     public int         dayNumber;
     public List<Visit> visits   = new ArrayList<>();
     public int         totalTravelMin;
     public String      startTime;
     public String      endTime;
-
     public DayPlan(int dayNumber) {
         this.dayNumber = dayNumber;
         this.startTime = "8:00 AM";
     }
-
     public int visitCount() { return visits.size(); }
 }
