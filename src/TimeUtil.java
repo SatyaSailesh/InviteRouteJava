f7f832fc90fa30bdf9d145285ef80981c7a21532
Kotla Satya Sai Sailesh <kotla.sailesh2006@gmail.com>
2026-03-10 10:53:00 +0530

    Update TimeUtil.java

--- src/TimeUtil.java
+++ src/TimeUtil.java
@@ -1,11 +1,5 @@
-/**
- * TimeUtil.java
- * Converts raw minutes-from-8AM into readable time strings.
- */
 public class TimeUtil {
-
     private static final int START_HOUR = 8;
-
     public static String format(double minutesFrom8AM) {
         int totalMins = (int) Math.round(START_HOUR * 60 + minutesFrom8AM);
         int h = (totalMins / 60) % 24;
@@ -14,8 +8,6 @@ public class TimeUtil {
         int    hDisp  = h > 12 ? h - 12 : (h == 0 ? 12 : h);
         return String.format("%d:%02d %s", hDisp, m, ampm);
     }
-
-    /** Travel time in minutes for a given Euclidean distance at 40 units/hr */
     public static double travelMin(double distance) {
         return (distance / 40.0) * 60.0;
     }
